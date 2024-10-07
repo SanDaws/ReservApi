a8f21edd6113febb8acbdb7ef0a8c47858880448
SanDaws <castrogiraldosantiago@gmail.com>
2024-10-07 08:54:49 -0500

    Feat: conection dbContext

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 
+using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
+using ReservApi.Data;
+
 namespace ReservApi;
 
 public class Program
@@ -8,6 +12,15 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
+
+        //dbContext
+        Env.Load();
+        var ServerModuleConection = $"server={Environment.GetEnvironmentVariable("HOST")};" +
+                                   $"port={Environment.GetEnvironmentVariable("PORT")};" +
+                                   $"database={Environment.GetEnvironmentVariable("DATABASE")};" +
+                                   $"uid={Environment.GetEnvironmentVariable("USER")};" +
+                                   $"password={Environment.GetEnvironmentVariable("PASSWORD")}";
+        builder.Services.AddDbContext<ReserverDbContext>(options => options.UseMySql(ServerModuleConection, ServerVersion.Parse("8.0.20-mysql")));// case using MySQl
 
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
